76c120350ab6f5b8de1d17a112e6c306f6b9092a
Markus Brueckner <namenlos@geekbetrieb.de>
2021-06-13 18:53:50 +0200

    code cleanup

--- src/model/state/uiState.ts
+++ src/model/state/uiState.ts
@@ -1,14 +1,6 @@
 import { log } from './log';
 import createHook from 'zustand';
 import create from 'zustand/vanilla';
-import { Theme } from '../../style/theme';
-import { darkTheme } from '../../style/dark-theme';
-import { lightTheme } from '../../style/light-theme';
-import { darkBlueTheme } from '../../style/dark-blue-theme';
-import { lightBlueTheme } from '../../style/light-blue-theme';
-import { darkRedTheme } from '../../style/dark-red-theme';
-import { lightRedTheme } from '../../style/light-red-theme';
-import { Logger } from '../../util/logger';
 
 type UiActions = {
     startProgress: (name: string) => void;
