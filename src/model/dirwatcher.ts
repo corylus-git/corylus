4d0cc88f8786662303f37aa126ad6b1569107af7
Markus Brueckner <namenlos@geekbetrieb.de>
2021-12-16 23:07:09 +0100

    use chokidar for repo dir watching

--- src/model/dirwatcher.ts
+++ src/model/dirwatcher.ts
@@ -1,32 +1,42 @@
-import fs from 'fs';
+import path from 'path';
+import chokidar from 'chokidar';
 import { Subject } from 'rxjs';
 import React from 'react';
 import { throttleTime } from 'rxjs/operators';
 import { Logger } from '../util/logger';
 import { repoStore } from './state/repo';
 
-export function useDirWatcher(path: string): void {
+export function useDirWatcher(repoPath: string): void {
     React.useEffect(() => {
-        if (path !== '') {
-            if (process.platform === 'win32') {
-                Logger().warn(
-                    'dirWatcher',
-                    'Disabling dir watcher on  windows, as there are performance issues still unresolved.'
-                );
-                return;
-            }
-            Logger().debug('dirWatcher', `Setting up directory watcher for ${path}`);
-            const subj = new Subject<any>();
-            let watcher: fs.FSWatcher | undefined = undefined;
+        if (repoPath !== '') {
+            let watcher: chokidar.FSWatcher | undefined = undefined;
             let interval: number | undefined = undefined;
+            let subscription: any;
             try {
-                watcher = fs.watch(path, (eventType, filename) => {
-                    Logger().silly('dirWatcher', 'Received event', {
-                        type: eventType,
-                        file: filename,
+                Logger().debug('dirWatcher', `Setting up directory watcher for ${repoPath}`);
+                const subj = new Subject<any>();
+                watcher = chokidar
+                    .watch(repoPath, {
+                        ignored: path.join(repoPath, '.git'),
+                    })
+                    .on('all', (ev, p) => {
+                        Logger().silly('dirWatcher', 'Received event', {
+                            type: ev,
+                            file: p,
+                        });
+                        subj.next(p);
                     });
-                    subj.next(filename);
-                });
+                subscription = subj
+                    .pipe(
+                        throttleTime(5000, undefined, { leading: true, trailing: true }) // ensure that the callback doesn't fire too often
+                    )
+                    .subscribe((files) => {
+                        Logger().debug('dirWatcher', 'Directory changed. Reloading repo.', {
+                            path: repoPath,
+                            files: files,
+                        });
+                        repoStore.getState().getStatus();
+                    });
             } catch (e) {
                 Logger().error('dirWatcher', 'Could not initialize directory watcher.', {
                     error: e,
@@ -34,23 +44,12 @@ export function useDirWatcher(path: string): void {
                 Logger().debug('dirWatcher', 'Falling back to polling');
                 interval = setInterval(() => repoStore.getState().getStatus(), 60_000);
             }
-            const subscription = subj
-                .pipe(
-                    throttleTime(5000, undefined, { leading: true, trailing: true }) // ensure that the callback doesn't fire too often
-                )
-                .subscribe((files) => {
-                    Logger().debug('dirWatcher', 'Directory changed. Reloading repo.', {
-                        path: path,
-                        files: files,
-                    });
-                    repoStore.getState().getStatus();
-                });
             return () => {
-                Logger().debug('dirWatcher', `Disabling directory watcher for ${path}`);
+                Logger().debug('dirWatcher', `Disabling directory watcher for ${repoPath}`);
                 subscription.unsubscribe();
                 watcher?.close();
                 interval && clearInterval(interval);
             };
         }
-    }, [path]);
+    }, [repoPath]);
 }
